cfe151bf20d0d22ff681fa82236d26d205354f02
WizedWasTaken <141151679+WizedWasTaken@users.noreply.github.com>
2024-02-08 13:01:55 +0100

    Refactor code to use const instead of let

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
-"use strict";
-
 function opgave1() {
-  let menuElement = document.querySelector("#menu");
+  const menuElement = document.querySelector("#menu");
   console.dir(menuElement);
 }
 
 function opgave2() {
-  let activeElement = document.querySelector(".active");
+  const activeElement = document.querySelector(".active");
   console.dir(activeElement);
 }
 
 function opgave3() {
-  let headingOneElementsFromHTML = document.querySelectorAll("h1");
+  const headingOneElementsFromHTML = document.querySelectorAll("h1");
   console.dir(headingOneElementsFromHTML);
 }
 
 function opgave4() {
-  let paragraphElementsFromHTML = document.querySelectorAll("p");
+  const paragraphElementsFromHTML = document.querySelectorAll("p");
   console.dir(paragraphElementsFromHTML);
 }
 
 function opgave5() {
-  let imageElements = document.querySelectorAll("img");
+  const imageElements = document.querySelectorAll("img");
   imageElements.forEach((imageElement) => {
     imageElement.alt = "Billedet er underlagt copyright";
   });
 }
 
 function opgave6() {
-  let firstHeadingElement = document.querySelector("h1");
+  const firstHeadingElement = document.querySelector("h1");
   firstHeadingElement.textContent = "Overskriften er udskiftet";
 }
 
 function opgave7() {
-  let headingElements = document.querySelectorAll("h1");
+  const headingElements = document.querySelectorAll("h1");
   headingElements.forEach((headingElement) => {
     headingElement.classList.add("title");
   });
 }
 
 function opgave8() {
-  let listItems = document.querySelectorAll("li");
+  const listItems = document.querySelectorAll("li");
   console.dir(listItems);
 }
 
 function opgave9() {
-  let playgroundElement = document.querySelector("#playground");
+  const playgroundElement = document.querySelector("#playground");
 
-  let unorderedListElement = document.createElement("ul");
+  const unorderedListElement = document.createElement("ul");
 
   for (let i = 0; i < 3; i++) {
     let listItemElement = document.createElement("li");
@@ -58,31 +56,48 @@ function opgave9() {
   playgroundElement.appendChild(unorderedListElement);
 }
 
+// function opgave10() {
+//   let imageElements = document.querySelectorAll("img");
+//   imageElements[0].src = imageElements[1].src;
+//   imageElements[1].src = imageElements[0].src;
+// }
+
 function opgave10() {
-  let imageElements = document.querySelectorAll("img");
+  const imageElements = document.querySelectorAll("img");
+  for (let i = 0; i < imageElements.length; i += 2) {
+    // Hvis der er et ulige antal billeder, så vil det sidste billede ikke blive byttet.
+    if (i + 1 < imageElements.length) {
+      // Opret midlertidige placeholders i DOM'en for at reservere plads til billedbyttet.
+      const placeholder1 = document.createElement("div");
+      const placeholder2 = document.createElement("div");
 
-  let nextImageParent = imageElements[1].parentNode;
-  for (let i = 0; i < imageElements.length; i + 2) {
-    console.log("i: " + i);
-    console.log(imageElements);
-    let currentImage = imageElements[i];
-    let nextImage = imageElements[i + 1];
+      // Find billederne der skal skiftes, samt deres parent element.
+      const firstImage = imageElements[i];
+      const secondImage = imageElements[i + 1];
+      const firstParent = firstImage.parentNode;
+      const secondParent = secondImage.parentNode;
 
-    if (nextImage != undefined) {
-      //   console.log("True: " + i);
-      //   console.log(nextImage);
-      nextImageParent = nextImage.parentNode;
-      //   console.log("nextImageParent: ", nextImageParent);
-      nextImageParent.insertBefore(currentImage, nextImage);
-    } else {
-      nextImage = imageElements[i - 1];
-      //   console.log("False: " + i);
-      //   console.log(nextImage);
+      // Put placeholdersne ind i DOM'en for at reservere plads.
+      firstParent.insertBefore(placeholder1, firstImage);
+      secondParent.insertBefore(placeholder2, secondImage);
 
-      nextImage.parentNode.insertBefore(nextImage, currentImage);
-      currentImage.remove();
+      // Skift de 2 billeder ved at flytte dem før vores placeholders.
+      firstParent.insertBefore(secondImage, placeholder1);
+      secondParent.insertBefore(firstImage, placeholder2);
+
+      // Fjern placeholdersne igen.
+      placeholder1.remove();
+      placeholder2.remove();
     }
   }
+}
+
+function opgave11() {
+  const pElements = document.querySelectorAll("#deleteme, #deletemetoo");
+
+  pElements.forEach((pElement) => {
+    pElement.remove();
+  });
 }
 
 opgave10();
